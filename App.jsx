3a1ca08743ce0e898270f336b7218a5453241c98
hju <guddls654@naver.com>
2021-03-27 10:11:07 +0900

    first commit

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigator from './navigations/StackNavigator';
+
+export default function App() {
+  console.disableYellowBox = true;
+
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
